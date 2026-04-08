359144cf3c91b229a8aa95fa940ac314c29cfdd4
anuraj-2000 <anurajmunnad@gmail.com>
2026-04-08 19:16:11 +0530

    Modified CORS setup and server listening port for render

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
-
 const bookRoutes = require("./routes/books");
-
 app.use("/books", bookRoutes);
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
