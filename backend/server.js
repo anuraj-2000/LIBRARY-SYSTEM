fe106c6d1c8826788ab5fa9a744253f158d3a2f8
anuraj-2000 <anurajmunnad@gmail.com>
2026-04-08 20:32:16 +0530

    Edited CORS configuration after failure mail

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,13 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
-app.use(cors({
-  origin: "*"
-}));
-
+app.use(cors());
 app.use(express.json());
-const bookRoutes = require("./routes/books");
-app.use("/books", bookRoutes);
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const bookRoutes = require("./routes/books");
+
+app.use("/books", bookRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
