0a942377b14ccc0774c9dd5647fa5624706b7f8d
anuraj-2000 <anurajmunnad@gmail.com>
2026-04-08 19:24:00 +0530

    Removed devolping setups

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()], 
-  server: {
-    port: 5173,
-    proxy: {
-      "/books": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
+  
 })
