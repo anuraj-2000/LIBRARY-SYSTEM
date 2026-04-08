2336cacbf10d461674202e2d9b37431238d3e17c
anuraj-2000 <anurajmunnad@gmail.com>
2026-04-08 17:31:07 +0530

    connect render database

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "127.0.0.1",
-  database: "library",
-  password: "1234",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-pool.connect()
-  .then(() => console.log("PostgreSQL Connected"))
-  .catch(err => console.error("Connection error", err));
 
 module.exports = pool;
